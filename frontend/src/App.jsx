7085f385c03450400fd7267da1b7889c73b30593
Alan <a.souzaaa@gmail.com>
2026-04-14 15:45:45 -0300

    fix: adicionar .node-version=20 e remover import não usado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { getMeses } from './api'
-import { mesAtual } from './utils/date'
 import { ToastProvider } from './components/Toast'
 import Header from './components/Header'
 import KPICards from './components/KPICards'
